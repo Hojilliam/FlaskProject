80e094ae77a596029fa0316cbd50f535d1d1e430
Hojilliam <kimovjordan@gmail.com>
2024-12-23 18:14:14 +0100

    integrated new filled database with code, commented unnecessary code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ except Exception as e:
 app = Flask(__name__)
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_vouchers.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database with the app
@@ -24,13 +24,13 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    new_user = User(**data)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify(new_user.to_dict()), 201
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     data = request.get_json()
+#     new_user = User(**data)
+#     db.session.add(new_user)
+#     db.session.commit()
+#     return jsonify(new_user.to_dict()), 201
 
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
@@ -42,18 +42,18 @@ def get_users():
     users = User.query.all()
     return jsonify([user.to_dict() for user in users])
 
-@app.route("/users/<int:user_id>", methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({'message': 'User deleted!'})
+# @app.route("/users/<int:user_id>", methods=['DELETE'])
+# def delete_user(user_id):
+#     user = User.query.get_or_404(user_id)
+#     db.session.delete(user)
+#     db.session.commit()
+#     return jsonify({'message': 'User deleted!'})
 
-@app.route("/total_spent/<int:user_id>", methods=['GET'])
-def get_total_spent(user_id):
-    total = User.query.get_or_404(user_id)
-    user_data = {'total_spent': total.total_spent, 'id': total.id}
-    return jsonify(user_data)
+# @app.route("/total_spent/<int:user_id>", methods=['GET'])
+# def get_total_spent(user_id):
+#     total = User.query.get_or_404(user_id)
+#     user_data = {'total_spent': total.total_spent, 'id': total.id}
+#     return jsonify(user_data)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,18 +1,29 @@
 from extensions import db
 
 class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'user_info'
+    user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    surname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    total_spent = db.Column(db.Integer)
 
     def to_dict(self):
         return {
-            'id': self.id,
+            'user_id': self.user_id,
             'name': self.name,
-            'surname': self.surname,
-            'age': self.age,
-            'total_spent': self.total_spent
+            'email': self.email,
+            'age': self.age
+        }
+
+class Spending(db.Model):
+    __tablename__ = 'user_spending'
+    user_id = db.Column(db.Integer, db.ForeignKey('user_info.user_id'), primary_key=True)
+    money_spent = db.Column(db.Integer)
+    year = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'money_spent': self.money_spent,
+            'year': self.year
         }
