70215cb02170df3de8bbaf51a83945539b0a6377
Hojilliam <kimovjordan@gmail.com>
2025-01-08 18:12:16 +0100

    overhauled get_avg_by_age

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from extensions import db
 from extensions import client
 from models import User, Spending
 from sqlalchemy import inspect, and_
-from functions import get_avg
 
 # Send a ping to confirm a successful connection
 try:
@@ -41,7 +40,6 @@ def get_users():
 def get_total_spent(user_id):
 
     spending = Spending.query.filter_by(user_id=user_id).all()
-
     print(spending)
 
     for spent in spending:
@@ -58,54 +56,47 @@ def get_total_spent(user_id):
 
     return jsonify(data)
 
-# To be modified/shortened again and checked for accuracy
+# To be checked for accuracy
 @app.route('/average_spending_by_age', methods=['GET'])
 def get_avg_by_age():
-    users18_24 = User.query.filter(and_(User.age >= 18, User.age <= 24)).all()
-    users18_24_list = []
-    for user in users18_24:
-        users18_24_list.append(user.user_id)
 
-    age18_24 = get_avg(users18_24_list)
-# --------------------------------------
-    users25_30 = User.query.filter(and_(User.age >= 25, User.age <= 30)).all()
-    users25_30_list = []
-    for user in users25_30:
-        users25_30_list.append(user.user_id)
+    def get_avg(users):
+        users_spending = 0
+        counter = 0
+        for user_id in users:
+            iterator = Spending.query.filter_by(user_id=user_id).all()
+            for value in iterator:
+                users_spending += value.money_spent
+                counter += 1
+        return users_spending / counter
 
-    age25_30 = get_avg(users25_30_list)
-# --------------------------------------
-    users31_36 = User.query.filter(and_(User.age >= 31, User.age <= 36)).all()
-    users31_36_list = []
-    for user in users31_36:
-        users31_36_list.append(user.user_id)
+    def get_users_by_age_range(min_age, max_age=None):
+        if max_age:
+            return User.query.filter(and_(User.age >= min_age, User.age <= max_age)).all()
+        else:
+            return User.query.filter(User.age >= min_age).all()
 
-    age31_36 = get_avg(users31_36_list)
-# --------------------------------------
-    users37_47 = User.query.filter(and_(User.age >= 37, User.age <= 47)).all()
-    users37_47_list = []
-    for user in users37_47:
-        users37_47_list.append(user.user_id)
+    def calculate_avg_spending(users):
+        user_ids = [user.user_id for user in users]
+        return get_avg(user_ids)
 
-    age37_47 = get_avg(users37_47_list)
-# --------------------------------------
-    users47 = User.query.filter(User.age > 47).all()
-    users47_list = []
-    for user in users47:
-        users47_list.append(user.user_id)
+    age_ranges = [
+        (18, 24),
+        (25, 30),
+        (31, 36),
+        (37, 47),
+        (47, None)
+    ]
 
-    age47 = get_avg(users47_list)
+    avg_spending_by_age = {}
+    for min_age, max_age in age_ranges:
+        users = get_users_by_age_range(min_age, max_age)
+        avg_spending = calculate_avg_spending(users)
+        age_key = f'{min_age} - {max_age}' if max_age else f'{min_age}+'
+        avg_spending_by_age[age_key] = round(avg_spending, 2)
 
+    return jsonify(avg_spending_by_age)
 
-    data = {
-        'age 18 - 24': round(age18_24, 3),
-        'age 25 - 30': round(age25_30, 3),
-        'age 31 - 36': round(age31_36, 3),
-        'age 37 - 47': round(age37_47, 3),
-        'age 47+': round(age47, 3)
-    }
-
-    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
